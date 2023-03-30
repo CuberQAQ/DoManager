188c4ef2c4bb15620979799dfbdbc2ad8317960b
CuberQAQ <a224503353@163.com>
2023-03-30 17:29:31 +0800

    upload

---
+++ index.js
@@ -0,0 +1,45 @@
+import fs from "fs-extra";
+import express from "express";
+import bodyParser from "body-parser";
+import jsonfile from "jsonfile";
+
+const port = 32386;
+const app = express();
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const file_now_pes = "./data/now_pes.json";
+
+const moduleName = "CuberEngine FileServer";
+const moduleVersion = "0.1.0";
+console.log(moduleName + " (" + moduleVersion + ") Start...");
+console.log("Listening on Port: " + port);
+
+app.get("/now_pes", (req, res) => {
+  let obj = jsonfile.readFileSync(file_now_pes, {
+    encoding: "utf8",
+    flag: "r",
+  });
+  console.log("GET now_pes:" + JSON.stringify(obj));
+  res.status(200).json(obj);
+});
+
+app.post("/now_pes", (req, res) => {
+  console.log("POST now_pes:" + req.body);
+  jsonfile.writeFileSync(file_now_pes, JSON.parse(req.body));
+  let obj = jsonfile.readFileSync(file_now_pes, {
+    encoding: "utf8",
+    flag: "r",
+  });
+  res.status(200).json(obj);
+});
+
+// Start Listening on Port
+app.listen(port);
